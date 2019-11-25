aa67f4325f1585527ea0a899aca8b2eeac1bab18
Smalik123456 <shamaeel.malik@riarauniversity.ac.ke>
2019-11-26 00:12:35 +0300

    Initial commit

---
+++ src/Division.kt
@@ -0,0 +1,13 @@
+fun main(args:Array<String>) {
+    val first: Double = 10.0
+    val second: Double = 5.0
+    val divide: Double = first / second
+    println(divide)
+    floatFunction()
+}
+fun floatFunction() {
+    val a: Float = 10.0000f
+    val b: Float = 10.000f
+    val ans: Float = b % a
+    println("The modulus of b and a is : " + ans)
+}
